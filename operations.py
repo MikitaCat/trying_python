3c0a2730c4f72da6a981b6af0e5250852722dcf4
MikitaCat <nikita.proger21@gmail.com>
2021-12-15 12:19:35 +0300

    Simple math operations

---
+++ operations.py
@@ -0,0 +1,21 @@
+# +, -, *, **, %, unary -, rounding, Pi
+number1 = 5
+number2 = 10
+
+sum = number1 + number2
+print(sum)
+
+sub = number2 - number1
+print(sub)
+
+mult = number1 * number2
+print(mult)
+
+division = number2 / number1
+print(division)
+
+expo = number2 ** number1
+print(expo)
+
+modulo = number2 % 3
+print(modulo)
