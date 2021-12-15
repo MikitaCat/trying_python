08f6f1090c510fca1b1c30a4156ef59d3ba0c4a3
MikitaCat <nikita.proger21@gmail.com>
2021-12-15 12:31:35 +0300

    Numbers rounding and 'math' import

--- operations.py
+++ operations.py
@@ -1,6 +1,9 @@
 # +, -, *, **, %, unary -, rounding, Pi
+import math
+
 number1 = 5
 number2 = 10
+number3 = 5.65
 
 sum = number1 + number2
 print(sum)
@@ -19,3 +22,15 @@ print(expo)
 
 modulo = number2 % 3
 print(modulo)
+
+round = round(number3)
+print(round)
+
+round_floor = math.floor(number3)
+print(round_floor)
+
+round_ceil = math.ceil(number3)
+print(round_ceil)
+
+number_pi = math.pi
+print(number_pi)
